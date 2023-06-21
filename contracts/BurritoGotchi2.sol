f170dc4f2d66af61d160242a92d4165397b3105e
alannetwork <alanestrada@cloudmex.io>
2023-06-21 09:54:52 -0700

    Last version with enumarables

--- contracts/BurritoGotchi2.sol
+++ contracts/BurritoGotchi2.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
-contract VirtualPet is ERC721URIStorage {
+contract BurritoBattleVP is ERC721URIStorage, ERC721Enumerable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -23,7 +23,7 @@ contract VirtualPet is ERC721URIStorage {
 
     mapping(uint256 => Pet) private _pets;
 
-    constructor() ERC721("VirtualPet", "VPET") {}
+    constructor() ERC721("Burrito Battle Virtual Pet", "BBPET") {}
 
     function mintPet(string memory petName) external returns (uint256) {
         _tokenIds.increment();
@@ -118,5 +118,41 @@ contract VirtualPet is ERC721URIStorage {
         }
         return string(buffer);
     }
+
+
+
+ // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override( ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+ 
+ 
 }
 
